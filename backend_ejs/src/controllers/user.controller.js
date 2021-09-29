b4f15315b7a6294a340b47ceed38323e861449b7
Sumit Singh <sumit47919@gmail.com>
2021-09-29 22:25:11 +0530

    Signup

--- backend_ejs/src/controllers/user.controller.js
+++ backend_ejs/src/controllers/user.controller.js
@@ -10,10 +10,8 @@ router.get('',async(req,res)=>{
 
 router.post('',async(req,res)=>{
     const users = await User.create(req.body)
-    if(users.first_name == null){
-        res.render("First")
-    }
-    res.render("signin")
+    
+    return res.redirect("signin")
 })
 
 module.exports = router;
